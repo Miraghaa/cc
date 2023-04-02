6e8679dc0690fbbcc44a42ed6b8df8690faa8d62
Miraghaa <miraghaea@code.edu.az>
2023-04-02 23:08:02 +0400

    Programm2.cs

---
+++ Programm2.cs
@@ -0,0 +1,21 @@
+﻿int[] numbers = { 15, -20, 5, 7, -5, -6, -7, 87, 90, 64 };
+int i = 0;
+int musbet = 0;
+int menfi = 0;
+while (i < numbers.Length)
+
+{
+    if (numbers[i] > 0)
+    {
+        musbet++;
+    }
+    else
+    {
+        menfi++;
+
+    }
+    i++;
+
+}
+Console.WriteLine(musbet);
+Console.WriteLine(menfi);
